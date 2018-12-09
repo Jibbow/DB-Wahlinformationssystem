9052470f54df169ef5b3fee7b9cedf7521c06b12
Andreas Zimmerer <andreas.zimmerer@tum.de>
2018-12-09 13:58:33 +0100

    Display map of Bayern in Frontend; testing c3.js charts

---
+++ WIS/web/src/views/LandtagView/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import C3Chart from 'react-c3js';
+import bayern_map from '../../assets/Bayern_Landtagswahlkreise_2018.svg';
+
+const data = {
+  columns: [
+    ['data1', 30],
+    ['data2', 70]
+  ],
+  type: 'donut'
+};
+
+
+export default class LandtagView extends Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <img src={bayern_map} className="bayern-map" alt="Karte von Bayern" />
+                </div>
+                <div>
+                    <C3Chart data={data} />
+                </div>
+            </div>
+        );
+    }
+}
