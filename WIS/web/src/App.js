7ee0e1326a399b676a94f8eca4726c17f30c8d43
Andreas Zimmerer <andreas.zimmerer@tum.de>
2018-12-09 16:05:25 +0100

    display charts with chart.js

--- WIS/web/src/App.js
+++ WIS/web/src/App.js
@@ -1,24 +1,23 @@
 import React, { Component } from 'react';
 import { Tabs, Tab } from 'react-bootstrap';
 import './App.css';
-import 'c3/c3.css';
 import LandtagView from './views/LandtagView'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Tabs defaultActiveKey={2} id="uncontrolled-tab-example">
+        <Tabs defaultActiveKey={1} id="uncontrolled-tab-example">
           <Tab eventKey={1} title="Überblick Landtagswahl">
             <LandtagView/>
           </Tab>
-          <Tab eventKey={2} title="Tab 2">
+          <Tab eventKey={2} title="Überblick Wahlkreise">
             Tab 2 content
           </Tab>
-          <Tab eventKey={3} title="Tab 3">
+          <Tab eventKey={3} title="Überblick Stimmkreise">
             Tab 3 content
           </Tab>
-        </Tabs>;
+        </Tabs>
       </div>
     );
   }
--- WIS/web/src/views/LandtagView/index.js
+++ WIS/web/src/views/LandtagView/index.js
@@ -1,27 +1,82 @@
-import React, { Component } from 'react'
-import C3Chart from 'react-c3js';
+import React, { Component } from 'react';
+import { Doughnut, Bar } from 'react-chartjs-2';
 import bayern_map from '../../assets/Bayern_Landtagswahlkreise_2018.svg';
-
-const data = {
-  columns: [
-    ['data1', 30],
-    ['data2', 70]
-  ],
-  type: 'donut'
-};
 
 
 export default class LandtagView extends Component {
+    constructor(props) {
+    super(props);
+    this.state = {
+      sitzverteilung: {
+          data: {
+              datasets: [
+                  {
+                      data: [10, 20, 30],
+                      backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f"],
+                  }
+              ],
+              labels: [
+                  'CSU',
+                  'SPD',
+                  'FDP'
+              ]
+          },
+          options: {
+            rotation: 1 * Math.PI,
+            circumference: 1 * Math.PI
+          }
+      },
+      stimmverteilung: {
+          data: {
+              datasets: [
+                  {
+                      label: "2018",
+                      data: [20.5, 13.2, 5.0],
+                      backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f"],
+                  },
+                  {
+                      label: "2013",
+                      data: [40.5, 15.2, 3.4],
+                      backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f"],
+                  }
+              ],
+              labels: [
+                  'CSU',
+                  'SPD',
+                  'FDP'
+              ]
+          },
+          options: {
+
+          }
+      }
+    };
+  }
+
     render() {
         return (
-            <div>
-                <div class="container">
+            <div class="row">
+                <div class="col-xs-6">
                     <img src={bayern_map} className="bayern-map" alt="Karte von Bayern" />
                 </div>
-                <div class="container-fluid">
-                    <C3Chart data={data} />
+                <div class="col-xs-6">
+                    <h2>Sitzverteilung im Landtag</h2>
+                    <Doughnut width={600} data={this.state.sitzverteilung.data} options={this.state.sitzverteilung.options}/>
+
+                    <h2>Ergebnisse der Parteien im Vergleich</h2>
+                    <Bar width={600} data={this.state.stimmverteilung.data} options={this.state.sitzverteilung.options}/>
+
+                    <h2>Mitglieder im Landtag</h2>
+                    
+                    <h2>Knappste Sieger in den Stimmkreisen</h2>
                 </div>
             </div>
         );
+    }
+
+    componentDidMount() {
+    fetch('http://localhost:8000/hello')
+      .then(response => response.json())
+      .then(data => this.setState({ sitzplatzverteilung: data }));
     }
 }
