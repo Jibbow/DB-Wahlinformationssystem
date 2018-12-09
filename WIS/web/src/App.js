aa511fb6be8cf5943ba34018f96e29cd5af7b156
Andreas Zimmerer <andreas.zimmerer@tum.de>
2018-12-09 11:26:19 +0100

    switched to create-react-app for frontend

--- WIS/web/src/App.js
+++ WIS/web/src/App.js
@@ -1,30 +1,27 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      result: 0,
-    };
-    this.getResult = this.getResult.bind(this);
-  }
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <p>{this.state.result}</p>
-          <button onClick={this.getResult}>click!</button>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
         </header>
       </div>
     );
-  }
-
-  getResult() {
-    this.setState({
-      result: 'yeah!',
-    });
   }
 }
 
