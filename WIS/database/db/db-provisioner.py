0f70f3029006afea7e9a3eee22aac7b4dbfb54bb
Andreas Zimmerer <andreas.zimmerer@tum.de>
2019-01-01 21:50:46 +0100

    Revert "some queries are now based on stored procedures"
    This reverts commit 30047dc1401309c6425a89cdf9915f6ee0e64960.

--- WIS/database/db/db-provisioner.py
+++ WIS/database/db/db-provisioner.py
@@ -55,6 +55,7 @@ def setup_schemas(connection):
                     except Exception as e:
                         print(e.args)
             cursor.close()
+    print('...done\n')
 
 
 
@@ -72,6 +73,7 @@ def setup_stored_procs(connection):
                 except Exception as e:
                     print(e.args)
             cursor.close()
+    print('...done\n')
 
 
 
@@ -91,17 +93,18 @@ def load_data(connection):
                     csv_file  = io.TextIOWrapper(csv_binary_file)
                     tablename = os.path.splitext(filename)[0].split('__')[1]
                     load_csv_file(connection, csv_file, tablename)
+    print('...done\n')
 
 
 
 ## Load Schema definitions
 setup_schemas(connection)
 
-## Load Data
-load_data(connection)
-
 ## Create Stored Procedures
 setup_stored_procs(connection)
+
+## Load Data
+load_data(connection)
 
 
 connection.commit()
